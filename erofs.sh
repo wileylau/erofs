ef6198e6dd88519bdaa095a33b01b567e517c5df
Wiley Lau <henloboii@protonmail.ch>
2021-10-05 16:18:52 +0800

    erofs: add support for non-system images
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- erofs.sh
+++ erofs.sh
@@ -6,6 +6,7 @@ EXTRAOPT=$3
 
 NEWIMAGE="$PARTITION-ext4.img"
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+RUNDIR=$(realpath .)
 MOUNTDIR="$LOCALDIR/$PARTITION"
 toolsdir="$LOCALDIR/tools"
 tmpdir="$LOCALDIR/tmp"
@@ -39,15 +40,26 @@ contextfix() {
     echo "/my_stock                      u:object_r:rootfs:s0" >> "$fileconts"
     echo "/my_version                    u:object_r:rootfs:s0" >> "$fileconts"
     echo "/special_preload               u:object_r:rootfs:s0" >> "$fileconts"
+    if [[ $PARTITION != "system" ]]; then
+        mkdir $LOCALDIR/system
+        sudo mount -o loop -t erofs $RUNDIR/system.img $LOCALDIR/system
+        sudo cat $LOCALDIR/system/system/etc/selinux/plat_file_contexts >> $fileconts
+        sudo umount -f -l $LOCALDIR/system
+        rm -rf $LOCALDIR/system
+    fi
 }
 
 rebuild() {
     mkdir $tmpdir
     echo "Rebuilding $PARTITION as ext4 image..."
-    cp -fpr $(sudo find $MOUNTDIR | grep plat_file_contexts) $tmpdir/
+    cp -fpr $(sudo find $MOUNTDIR | grep file_contexts) $tmpdir/
     contextfix
     imagesize=`du -sk $MOUNTDIR | awk '{$1*=1024;$1=int($1*1.05);printf $1}'`
-    sudo $toolsdir/mkuserimg_mke2fs.py "$MOUNTDIR/" "$NEWIMAGE" ext4 "/" 4294967296 $fileconts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
+    if [[ $PARTITION == "system" ]]; then
+        sudo $toolsdir/mkuserimg_mke2fs.py "$MOUNTDIR/" "$NEWIMAGE" ext4 "/" 4294967296 $fileconts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
+    else
+        sudo $toolsdir/mkuserimg_mke2fs.py "$MOUNTDIR/" "$NEWIMAGE" ext4 "/$PARTITION" 4294967296 $fileconts -j "0" -T "1230768000" -L "$PARTITION" -I "256" -M "/$PARTITION" -m "0"
+    fi
     sudo umount -f -l $MOUNTDIR
     rm -rf $MOUNTDIR 
     sudo rm -rf $tmpdir
