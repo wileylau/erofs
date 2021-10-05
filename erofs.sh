b1efa34d0a1b3768dbb478d4e2ec0b6254c611ba
Wiley Lau <henloboii@protonmail.ch>
2021-10-05 14:55:26 +0800

    erofs: fix imagesize count
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- erofs.sh
+++ erofs.sh
@@ -29,7 +29,7 @@ rebuild() {
     mkdir $tmpdir
     echo "Rebuilding $PARTITION as ext4 image..."
     cp -fpr $(sudo find $MOUNTDIR | grep plat_file_contexts) $tmpdir/
-    imagesize=`du -sk $PARTITION | awk '{$1*=1024;$1=int($1*1.05);printf $1}'`
+    imagesize=`du -sk $MOUNTDIR | awk '{$1*=1024;$1=int($1*1.05);printf $1}'`
     sudo python $toolsdir/mkuserimg_mke2fs.py "$MOUNTDIR/" "$NEWIMAGE" ext4 "/" $imagesize $tmpdir/plat_file_contexts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
     sudo umount -f -l $MOUNTDIR
     rm -rf $MOUNTDIR 
