0cf7871705ae3d897f98157fa516ea82610aa4a0
Wiley Lau <henloboii@protonmail.ch>
2021-10-05 13:29:13 +0800

    erofs: separate tmp into a variable
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- erofs.sh
+++ erofs.sh
@@ -7,6 +7,7 @@ EXTRAOPT=$3
 NEWIMAGE=$PARTITION_ext4.img
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 toolsdir="$LOCALDIR/tools"
+tmpdir="$LOCALDIR/tmp"
 
 usage() {
     echo "sudo ./$0 <image path> <partition name>"
@@ -17,18 +18,19 @@ if [[ $1 == "" ]]; then
 fi
 
 mount() {
+    sudo rm -rf $tmpdir
     mkdir $PARTITION
     echo "Mounting $PARTITION..."
     sudo mount -t erofs -o loop $IMAGE $PARTITION 
 }
 
 rebuild() {
-    mkdir tmp
+    mkdir $tmpdir
     echo "Rebuilding $PARTITION as ext4 image..."
     cp -fpr $(sudo find | grep plat_file_contexts) tmp/
     sudo python $toolsdir/mkuserimg_mke2fs.py "$PARTITION/" "$NEWIMAGE" ext4 "/" 4096M tmp/plat_file_contexts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
     sudo umount -f -l $PARTITION
-    rm -rf $PARTITION/ tmp/
+    rm -rf $PARTITION/ $tmpdir
 }
 
 if [[ $3 == "-m" ]]; then # mount only
