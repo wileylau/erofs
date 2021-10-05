0d27dba23fa4809ad339528cdaef2c0cc2276879
Wiley Lau <henloboii@protonmail.ch>
2021-10-05 14:02:37 +0800

    erofs: add $MOUNTDIR var
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- erofs.sh
+++ erofs.sh
@@ -6,6 +6,7 @@ EXTRAOPT=$3
 
 NEWIMAGE=$PARTITION_ext4.img
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+MOUNTDIR="$LOCALDIR/$PARTITION"
 toolsdir="$LOCALDIR/tools"
 tmpdir="$LOCALDIR/tmp"
 
@@ -18,20 +19,20 @@ if [[ $1 == "" ]]; then
 fi
 
 mount() {
-    sudo rm -rf $tmpdir
-    mkdir $PARTITION
+    sudo rm -rf $tmpdir $MOUNTDIR
+    mkdir $MOUNTDIR
     echo "Mounting $PARTITION..."
-    sudo mount -t erofs -o loop $IMAGE $PARTITION 
+    sudo mount -t erofs -o loop $IMAGE $MOUNTDIR 
 }
 
 rebuild() {
     mkdir $tmpdir
     echo "Rebuilding $PARTITION as ext4 image..."
-    cp -fpr $(sudo find | grep plat_file_contexts) $tmpdir
+    cp -fpr $(sudo find $MOUNTDIR | grep plat_file_contexts) $tmpdir/
     imagesize=`du -sk $PARTITION | awk '{$1*=1024;$1=int($1*1.05);printf $1}'`
-    sudo python $toolsdir/mkuserimg_mke2fs.py "$PARTITION/" "$NEWIMAGE" ext4 "/" $imagesize tmp/plat_file_contexts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
-    sudo umount -f -l $PARTITION
-    rm -rf $PARTITION 
+    sudo python $toolsdir/mkuserimg_mke2fs.py "$MOUNTDIR/" "$NEWIMAGE" ext4 "/" $imagesize tmp/plat_file_contexts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
+    sudo umount -f -l $MOUNTDIR
+    rm -rf $MOUNTDIR 
     sudo rm -rf $tmpdir
 }
 
