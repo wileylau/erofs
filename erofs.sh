2d63779a871bc49ca6abb350d1c7ba27da9d85ea
Wiley Lau <henloboii@protonmail.ch>
2021-10-05 13:29:32 +0800

    erofs: remove tmpdir wtih sudo
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- erofs.sh
+++ erofs.sh
@@ -30,7 +30,8 @@ rebuild() {
     cp -fpr $(sudo find | grep plat_file_contexts) tmp/
     sudo python $toolsdir/mkuserimg_mke2fs.py "$PARTITION/" "$NEWIMAGE" ext4 "/" 4096M tmp/plat_file_contexts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
     sudo umount -f -l $PARTITION
-    rm -rf $PARTITION/ $tmpdir
+    rm -rf $PARTITION 
+    sudo rm -rf $tmpdir
 }
 
 if [[ $3 == "-m" ]]; then # mount only
