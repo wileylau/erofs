0c9cbca1f0a6b9ecf7918596fea82a45329c71f6
Velosh <daffetyxd@gmail.com>
2021-10-05 13:17:26 +0800

    erofs: rebuild image with ext4
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- erofs.sh
+++ erofs.sh
@@ -3,6 +3,8 @@
 PARTITION=$1
 EXTRAOPT=$2
 
+NEWIMAGE=$PARTITION_ext4.img
+
 usage() {
     echo "sudo ./$0 <partition name>"
 }
@@ -21,7 +23,9 @@ rebuild() {
     mkdir tmp
     echo "Rebuilding $PARTITION as ext4 image..."
     cp -fpr $(sudo find | grep plat_file_contexts) tmp/
-    # TBC    
+    sudo tools/mkuserimg_mke2fs.py "$PARTITION/" "$NEWIMAGE" ext4 "/" 4096M tmp/plat_file_contexts -j "0" -T "1230768000" -L "/" -I "256" -M "/" -m "0"
+    sudo umount $PARTITION
+    rm -rf $PARTITION/
 }
 
 if [[ $2 == "-m" ]]; then # mount only
