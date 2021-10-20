466ecfc3a4724684a5e32ea4f25f439dda2ba7af
Wiley Lau <henloboii@protonmail.ch>
2021-10-20 15:48:53 +0800

    oplus-merge: only merge stuff that doesn't go to product
    * else it won't boot still.
    * and yea, oos12 is on boot animation :)
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- oplus-merge.sh
+++ oplus-merge.sh
@@ -15,8 +15,22 @@ merge() {
         echo "[INFO] Merging $partition into system"
         mkdir $partition >/dev/null 2>&1
         mount -o loop -t erofs $partition.img $partition  >/dev/null 2>&1
-        cd system >/dev/null 2>&1
-        cp -fpr ../$partition/ . >/dev/null 2>&1
+        cd system/$partition/ >/dev/null 2>&1
+        cp -fpr ../$partition/apkcerts.txt . >/dev/null 2>&1
+        cp -fpr ../$partition/applist . >/dev/null 2>&1
+        cp -fpr ../$partition/build.prop . >/dev/null 2>&1
+        cp -fpr ../$partition/custom_info.txt . >/dev/null 2>&1
+        cp -fpr ../$partition/decouping_wallpaper . >/dev/null 2>&1
+        cp -fpr ../$partition/del* . >/dev/null 2>&1
+        cp -fpr ../$partition/etc . >/dev/null 2>&1
+        cp -fpr ../$partition/framework . >/dev/null 2>&1
+        cp -fpr ../$partition/lost+found . >/dev/null 2>&1
+        cp -fpr ../$partition/media . >/dev/null 2>&1
+        cp -fpr ../$partition/non_overlay . >/dev/null 2>&1
+        cp -fpr ../$partition/plugin . >/dev/null 2>&1
+        cp -fpr ../$partition/product_overlay . >/dev/null 2>&1
+        cp -fpr ../$partition/res . >/dev/null 2>&1
+        cp -fpr ../$partition/vendor . >/dev/null 2>&1
         cd .. >/dev/null 2>&1
         umount -f -l $partition >/dev/null 2>&1
         rm -rf $partition/ >/dev/null 2>&1
